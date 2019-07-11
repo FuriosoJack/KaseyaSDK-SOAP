b69bfe2daf224b7bc7b649778ea5f8d166915257
FuriosoJack <iam@furiosojack.com>
2019-07-11 12:55:53 -0500

    Se añaden test para añadir scope

--- src/HTTP/DOM/Response/AddScopeResponseDOM.php
+++ src/HTTP/DOM/Response/AddScopeResponseDOM.php
@@ -8,7 +8,27 @@ namespace FuriosoJack\KaseyaSDKSOAP\HTTP\DOM\Response;
  * @package FuriosoJack\KaseyaSDKSOAP\HTTP\DOM\Response
  * @author Juan Diaz - FuriosoJack <iam@furiosojack.com>
  */
-class AddScopeResponseDOM
+class AddScopeResponseDOM extends BasicResponseDOM
 {
+
+    private $scopeOutId;
+    /**
+     * Metodo que se encarga de convertir los valores del dom en atributos del objeto
+     * @return mixed
+     */
+    protected function descompose()
+    {
+
+        $this->scopeOutId = $this->domDocument->getElementsByTagName("scopeOutId")->item(0)->textContent;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getScopeOutId()
+    {
+        return $this->scopeOutId;
+    }
+
 
 }
--- tests/Requests/SendTest.php
+++ tests/Requests/SendTest.php
@@ -42,14 +42,36 @@ class SendTest extends TestCase
             $response = $session->request(new GetOrgsRequestDOM($session->getAuthResponseDOM()->getSessionID()));
             $domResponse = $response->getResponseDOM();
 
-            var_dump($response->getBody());
-            var_dump($domResponse->getOrgs());
+            $this->assertTrue(count($domResponse->getOrgs()) > 0);
+
+        }else{
+            $this->assertTrue(false);
         }
+    }
 
+    public function testaddScope()
+    {
+        $credential = new Credentials(getenv("USERNAME_SERVER"),getenv("PASSWORD_SERVER"));
+        $session = new Session($credential,getenv('URL_SERVER'));
 
+        if($session->auth())
+        {
+            $addScope = new AddScopeRequestDOM("TESTCOP22E",$session->getAuthResponseDOM()->getSessionID() );
+            $response = $session->request($addScope);
+            if($response->getStatusCode() == 200){
+                $domResponse = $response->getResponseDOM();
+                //No debe existir ningun mensaje para que pase el test, puede ser que el scope ya este creado
+                $this->assertTrue(empty($domResponse->getErrorMessage()));
+                return;
+            }
 
+        }
+        $this->assertTrue(false);
 
 
     }
+
+
+
 
 }
